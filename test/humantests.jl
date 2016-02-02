ae8d9b311191363dfd972673777f170502422a4b
Evan Fields <=>
2016-02-02 13:09:59 -0500

    separate visualization in human tests

--- test/humantests.jl
+++ test/humantests.jl
@@ -4,11 +4,19 @@
 # requires several packages (such as Gadfly) not listed in the REQUIRE.
 using Gadfly
 using TravelingSalesmanHeuristics
+using Distances
 
-function visPlanarTSP(n = 10; extraArgs...)
-	pts = rand(2, n)
-	distMat = pairwise(Euclidean(), pts, pts)
-	path = nearestNeighbor(distMat; extraArgs...)
+# use Gadfly to plot a TSP visualization
+# pts must be a 2 x n matrix and path the vector of column indices
+function visTSP(pts, path)
 	pts = pts[:,path]
 	plot(x = pts[1,:], y = pts[2,:], Geom.path, Geom.point)
+end
+# visualize a planar TSP in the unit square
+# specify number of points and solution method
+function visPlanarTSP(n = 10, heuristic = nearestNeighbor; extraArgs...)
+	pts = rand(2, n)
+	distMat = pairwise(Euclidean(), pts, pts)
+	path, _ = heuristic(distMat; extraArgs...)
+	visTSP(pts, path)
 end
