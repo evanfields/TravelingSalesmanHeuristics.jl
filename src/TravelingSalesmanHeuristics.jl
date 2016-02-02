9cee0e5510814e7ce3d79b04d007636ce8a6db81
Evan Fields <=>
2016-02-02 09:45:15 -0500

    nearest neighbor heuristic, initial tests

--- src/TravelingSalesmanHeuristics.jl
+++ src/TravelingSalesmanHeuristics.jl
@@ -1,5 +1,107 @@
-module TravelingSalesmanHeuristics
+module TSPHeuristics
+using Distances
 
-# package code goes here
+export nearestNeighbor
+
+function nearestNeighbor{T<:Real}(distMat::Matrix{T};
+							   firstcity::Nullable{Int} = Nullable{Int}(),
+							   closepath = true,
+							   do2opt = true)
+	# must have a square matrix 
+	if size(distMat, 1) != size(distMat, 2)
+		error("Must pass a square distance matrix to nearestNeighbor")
+	end
+	numCities = size(distMat, 1)
+	
+	# put first city on path
+	path = Vector{Int}()
+	if isnull(firstcity)
+		firstcity = rand(1:numCities)
+	else
+		firstcity = get(firstcity)
+	end
+	push!(path, firstcity)
+	
+	# cities to visit
+	citiesToVisit = collect(1:(firstcity - 1))
+	append!(citiesToVisit, collect((firstcity + 1):numCities))
+	
+	# nearest neighbor loop
+	while !isempty(citiesToVisit)
+		curCity = path[end]
+		dists = distMat[curCity, citiesToVisit]
+		_, nextInd = findmin(dists)
+		nextCity = citiesToVisit[nextInd]
+		push!(path, nextCity)
+		deleteat!(citiesToVisit, nextInd)
+	end
+	
+	# complete cycle? (duplicates first city)
+	if closepath
+		push!(path, firstcity)
+	end
+	
+	# do swaps?
+	if do2opt
+		path = twoOpt(distMat, path)
+	end
+	
+	return path
+end
+
+function pathCost(distMat, path)
+	cost = 0
+	for i in 1:(length(path) - 1)
+		cost += distMat[path[i], path[i+1]]
+	end
+	return cost
+end
+
+
+
+function twoOpt{T<:Real}(distMat::Matrix{T}, path::Vector{Int})
+	# size checks
+	n = length(path)
+	if size(distMat, 1) != size(distMat, 2)
+		error("Distance matrix passed to twoOpt must be square.")
+	end
+	
+	# main loop
+	# check every possible switch until no 2-swaps reduce objective
+	# if the path passed in is a loop (first/last nodes are the same)
+	# then we must keep these the endpoints of the path the same
+	# ie just keep it a loop, and therefore it doesn't matter which node is at the end
+	# if the path is not a cycle, we can do any reversing we like
+	isCycle = path[1] == path[end]
+	switchLow = isCycle ? 2 : 1
+	switchHigh = isCycle ? n - 1 : n
+	prevCost = Inf
+	curCost = pathCost(distMat, path)
+	while prevCost > pathCost(distMat, path)
+		prevCost = curCost
+		# we can't change the first 
+		for i in switchLow:(n-1)
+			for j in (i+1):switchHigh
+				altPath = reverse(path, i, j)
+				altCost = pathCost(distMat, altPath)
+				if altCost < curCost
+					path = altPath
+					curCost = altCost
+				end
+			end
+		end
+	end
+	
+	return path
+end
+
+function visPlanarTSP(n = 10; extraArgs...)
+	pts = rand(2, n)
+	distMat = pairwise(Euclidean(), pts, pts)
+	path = nearestNeighbor(distMat; extraArgs...)
+	pts = pts[:,path]
+	plot(x = pts[1,:], y = pts[2,:], Geom.path, Geom.point)
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,73 @@
-using TravelingSalesmanHeuristics
+using TSPHeuristics
 using Base.Test
+using Gadfly
+using Distances
 
-# write your own tests here
-@test 1 == 1
+###
+# helpers
+###
+
+# generate a Euclidean distance matrix for n points in the unit square
+function generate_planar_distmat(n)
+	pts = rand(2, n)
+	return pairwise(Euclidean(), pts, pts)
+end
+
+# test that a path is acceptable:
+# - at most one city appears twice, and if so must be first and last
+# - all values 1:n are present where n is the maximum city index, and no other values
+function testpathvalidity(path, iscycle)
+	if iscycle
+		@test path[1] == path[end]
+		pop!(path)
+	end
+	path = sort(path)
+	for i in 1:length(path)
+		@test path[i] == i
+	end
+end
+
+# function for human tests: visualize TSP output
+# not to be run as part of automated tests
+function visPlanarTSP(n = 10; extraArgs...)
+	pts = rand(2, n)
+	distMat = pairwise(Euclidean(), pts, pts)
+	path = nearestNeighbor(distMat; extraArgs...)
+	pts = pts[:,path]
+	plot(x = pts[1,:], y = pts[2,:], Geom.path, Geom.point)
+end
+
+###
+# main tests
+###
+
+# test nearest neighbor heuristic
+function test_nearest_neighbor()
+	distmats = cell(2)
+	distmats[1] = generate_planar_distmat(30)
+	distmats[2] = generate_planar_distmat(2)
+	
+	for dm in distmats
+		n = size(dm, 1)
+		randstartcity = rand(1:n)
+		for firstcity in [Nullable{Int}(), Nullable(randstartcity)]
+			for do2opt in [true, false]
+				for closepath in [true, false]
+					path = nearestNeighbor(dm, firstcity = firstcity, 
+										do2opt = do2opt, closepath = closepath)
+					testpathvalidity(path, closepath)
+				end
+			end
+		end
+	end
+	
+	dm_bad = rand(3,2)
+	@test_throws ErrorException nearestNeighbor(dm_bad)
+end
+
+###
+# run
+###
+srand(47)
+test_nearest_neighbor()
+	
