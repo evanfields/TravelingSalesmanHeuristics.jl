334483d5c5d44b5aaa73735c6705138d1ef6220d
Evan Fields <ejf12009@mymail.pomona.edu>
2019-08-06 22:38:11 -0400

    fix our whitespace while we're here

--- src/simulated_annealing.jl
+++ src/simulated_annealing.jl
@@ -20,69 +20,69 @@ decays exponentially from `init_temp` to `final_temp`. Return a tuple `(path, co
 """
 function simulated_annealing(distmat::Matrix{T} where {T<:Real};
                              steps = 50*length(distmat),
-							 num_starts = 1,
-							 init_temp = exp(8), final_temp = exp(-6.5),
-							 init_path::Union{Vector{Int}, Nothing} = nothing)
+                             num_starts = 1,
+                             init_temp = exp(8), final_temp = exp(-6.5),
+                             init_path::Union{Vector{Int}, Nothing} = nothing)
 
-	# check inputs
-	n = check_square(distmat, "Must pass a square distance matrix to simulated_annealing.")
-	
-	# cooling rate: we multiply by a constant mult each step
-	cool_rate = (final_temp / init_temp)^(1 / (steps - 1))
+    # check inputs
+    n = check_square(distmat, "Must pass a square distance matrix to simulated_annealing.")
 
-	# do SA with a single starting path
-	function sahelper!(path)
-		temp = init_temp / cool_rate # divide by cool_rate so when we first multiply we get init_temp
-		n = size(distmat, 1)
-		cost_cur = pathcost(distmat, path)
+    # cooling rate: we multiply by a constant mult each step
+    cool_rate = (final_temp / init_temp)^(1 / (steps - 1))
 
-		for i in 1:steps
-			temp *= cool_rate
+    # do SA with a single starting path
+    function sahelper!(path)
+        temp = init_temp / cool_rate # divide by cool_rate so when we first multiply we get init_temp
+        n = size(distmat, 1)
+        cost_cur = pathcost(distmat, path)
 
-			# take a step
-			# keep first and last cities fixed
-			first, last = rand(2:n), rand(2:n)
-			if first > last
-				first, last = last, first
-			end
-			cost_other = pathcost_rev(distmat, path, first, last)
-			@fastmath accept = cost_other < cost_cur ? true : rand() < exp((cost_cur - cost_other) / temp)
-			# should we accept?
-			if accept
-				reverse!(path, first, last)
-				cost_cur = cost_other
-			end
-		end
+        for i in 1:steps
+            temp *= cool_rate
 
-		return path, cost_cur
-	end
+            # take a step
+            # keep first and last cities fixed
+            first, last = rand(2:n), rand(2:n)
+            if first > last
+                first, last = last, first
+            end
+            cost_other = pathcost_rev(distmat, path, first, last)
+            @fastmath accept = cost_other < cost_cur ? true : rand() < exp((cost_cur - cost_other) / temp)
+            # should we accept?
+            if accept
+                reverse!(path, first, last)
+                cost_cur = cost_other
+            end
+        end
 
-	# unpack the initial path
-	if init_path == nothing
-		randstart = true
-		path = randpath(n)
-	else
-		if !legal_circuit(init_path)
-			error("The init_path passed to simulated_annealing must be a legal circuit.")
-		end
-		randstart = false
-		path = init_path
-	end
-	cost = pathcost(distmat, path)
-	for _ in 1:num_starts
-		path_this_start = randstart ? randpath(n) : deepcopy(init_path)
-		otherpath, othercost = sahelper!(path_this_start)
-		if othercost < cost
-			cost = othercost
-			path = otherpath
-		end
-	end
+        return path, cost_cur
+    end
 
-	return path, cost
+    # unpack the initial path
+    if init_path == nothing
+        randstart = true
+        path = randpath(n)
+    else
+        if !legal_circuit(init_path)
+            error("The init_path passed to simulated_annealing must be a legal circuit.")
+        end
+        randstart = false
+        path = init_path
+    end
+    cost = pathcost(distmat, path)
+    for _ in 1:num_starts
+        path_this_start = randstart ? randpath(n) : deepcopy(init_path)
+        otherpath, othercost = sahelper!(path_this_start)
+        if othercost < cost
+            cost = othercost
+            path = otherpath
+        end
+    end
+
+    return path, cost
 end
 
 function randpath(n)
-	path = 1:n |> collect |> shuffle
-	push!(path, path[1]) # loop
-	return path
+    path = 1:n |> collect |> shuffle
+    push!(path, path[1]) # loop
+    return path
 end
